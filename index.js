c0aec61374b8bb2c92ae70fd16b8c3805bacb15d
albin <albinpirela40@gmail.com>
2022-12-09 09:49:29 -0400

    change my first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('Hola mundo')
+let miPrimeraVariable = 'mi primera variable!'
+console.log(miPrimeraVariable)
+miPrimeraVariable = 'esto a cambiado'
+console.log(miPrimeraVariable)
+let miBoolean = true
+let miOtroBool = false
+let miNumero = 0
+let miNumero2 = 12
+let miNumero3 = -250
+console.log(miNumero, miNumero2, miBoolean, miPrimeraVariable)
