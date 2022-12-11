7c9140d4af0d94c6e37cd7efe7992eb03c14b783
albin <albinpirela40@gmail.com>
2022-12-11 00:35:59 -0400

    my first commit

---
+++ albin.js
@@ -0,0 +1,16 @@
+//let myName = ("albin pirela");
+////console.log(myName)
+//////var myWife = ('Andreina AVila');
+//const myNameWife = myName + 'es esposo de ' + myWife;
+//console.log(myNameWife);//
+//let myBoolean = true//
+//let myOtroBool = false//
+///let miNumero = 0///
+//let miNumero2 = 11////////
+////let miNumero3 = 315////
+//let miNumero4 = -50//
+//console.log(miNumero, myBoolean, miNumero3 + miNumero2, myNameWife);//
+let undef
+console.log(undef);
+let nulo = null
+console.log(nulo, 13, null);
--- index.js
+++ index.js
@@ -8,4 +8,10 @@ let miOtroBool = false
 let miNumero = 0
 let miNumero2 = 12
 let miNumero3 = -250
-console.log(miNumero, miNumero2, miBoolean, miPrimeraVariable)
+//console.log(miNumero, miNumero2, miBoolean, miPrimeraVariable);
+let undef
+console.log(undef);
+let nulo = null
+console.log(nulo);
+//objeto
+//
